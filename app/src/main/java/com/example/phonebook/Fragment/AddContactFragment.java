7e2c2e77cbab21fbf0c0d4e02016a59279619bbd
DinhQuy <hdquy2003@gmail.com>
2025-06-08 01:54:07 +0700

    fix add contact

--- app/src/main/java/com/example/phonebook/Fragment/AddContactFragment.java
+++ app/src/main/java/com/example/phonebook/Fragment/AddContactFragment.java
@@ -1,10 +1,13 @@
 package com.example.phonebook.Fragment;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +31,7 @@ public class AddContactFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     LinearLayout containerPhone, containerEmail, containerNickname, containerURL, containerAddress, containerDoB, containerSocial, containerMessage;
     ImageView ivAddPhone, ivAddEmail, ivAddNickname, ivAddURL, ivAddAddress, ivAddDoB, ivAddSocial, ivAddMessage;
+    TextView tvAddCanel, tvAddComplete;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -74,116 +78,88 @@ public class AddContactFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        tvAddCanel = view.findViewById(R.id.tv_addcontact_cancel);
+        tvAddCanel.setOnClickListener(cancel -> {
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            fragmentManager.popBackStack();
+        });
+
+        tvAddComplete = view.findViewById(R.id.tv_addcontact_complete);
+        tvAddComplete.setOnClickListener(complete -> {
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            fragmentManager.popBackStack();
+        });
+
         containerPhone = view.findViewById(R.id.layout_phone);
         ivAddPhone = view.findViewById(R.id.add_btn_addphone);
         ivAddPhone.setOnClickListener(addPhone -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemPhone = inflater.inflate(R.layout.child_item,null);
-            TextView tvInput = itemPhone.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_phone));
-            ImageView ivDelete = itemPhone.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerPhone.removeView(itemPhone);
-            });
-            containerPhone.addView(itemPhone);
+            addAttribute(containerPhone, R.string.contact_phone);
         });
 
         containerEmail = view.findViewById(R.id.layout_email);
         ivAddEmail = view.findViewById(R.id.add_btn_addemail);
         ivAddEmail.setOnClickListener(addEmail -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemEmail = inflater.inflate(R.layout.child_item, null);
-            TextView tvInput = itemEmail.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_email));
-            ImageView ivDelete = itemEmail.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerEmail.removeView(itemEmail);
-            });
-            containerEmail.addView(itemEmail);
+            addAttribute(containerEmail, R.string.contact_email);
         });
 
         containerNickname = view.findViewById(R.id.layout_nickname);
         ivAddNickname = view.findViewById(R.id.add_btn_addnickname);
         ivAddNickname.setOnClickListener(addNickname -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemNickname = inflater.inflate(R.layout.child_item, null);
-            TextView tvInput = itemNickname.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_nickname));
-            ImageView ivDelete = itemNickname.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerNickname.removeView(itemNickname);
-            });
-            containerNickname.addView(itemNickname);
+            addAttribute(containerNickname, R.string.contact_nickname);
         });
 
         containerURL = view.findViewById(R.id.layout_url);
         ivAddURL = view.findViewById(R.id.add_btn_addurl);
         ivAddURL.setOnClickListener(addURL -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemURL = inflater.inflate(R.layout.child_item, null);
-            TextView tvInput = itemURL.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_url));
-            ImageView ivDelete = itemURL.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerURL.removeView(itemURL);
-            });
-            containerURL.addView(itemURL);
+            addAttribute(containerURL, R.string.contact_url);
         });
 
         containerAddress = view.findViewById(R.id.layout_address);
         ivAddAddress = view.findViewById(R.id.add_btn_addaddress);
         ivAddAddress.setOnClickListener(addAddress -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemAddress = inflater.inflate(R.layout.child_item, null);
-            TextView tvInput = itemAddress.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_address));
-            ImageView ivDelete = itemAddress.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerAddress.removeView(itemAddress);
-            });
-            containerAddress.addView(itemAddress);
+            addAttribute(containerAddress, R.string.contact_address);
         });
 
         containerDoB = view.findViewById(R.id.layout_dob);
         ivAddDoB = view.findViewById(R.id.add_btn_adddob);
         ivAddDoB.setOnClickListener(addDoB -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemDoB = inflater.inflate(R.layout.child_item, null);
-            TextView tvInput = itemDoB.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_dob));
-            ImageView ivDelete = itemDoB.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerDoB.removeView(itemDoB);
-            });
-            containerDoB.addView(itemDoB);
+            addAttribute(containerDoB, R.string.contact_dob);
         });
 
         containerSocial = view.findViewById(R.id.layout_social);
         ivAddSocial = view.findViewById(R.id.add_btn_addsocial);
         ivAddSocial.setOnClickListener(addSocial -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemSocial = inflater.inflate(R.layout.child_item, null);
-            TextView tvInput = itemSocial.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_social));
-            ImageView ivDelete = itemSocial.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerSocial.removeView(itemSocial);
-            });
-            containerSocial.addView(itemSocial);
+            addAttribute(containerSocial, R.string.contact_social);
         });
 
         containerMessage = view.findViewById(R.id.layout_message);
         ivAddMessage = view.findViewById(R.id.add_btn_addmessage);
         ivAddMessage.setOnClickListener(addMessage -> {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View itemMessage = inflater.inflate(R.layout.child_item, null);
-            TextView tvInput = itemMessage.findViewById(R.id.et_input_att);
-            tvInput.setHint(getString(R.string.contact_message));
-            ImageView ivDelete = itemMessage.findViewById(R.id.add_btn_delete_phone);
-            ivDelete.setOnClickListener(delete -> {
-                containerMessage.removeView(itemMessage);
-            });
-            containerMessage.addView(itemMessage);
+            addAttribute(containerMessage, R.string.contact_message);
         });
+    }
+
+    private void addAttribute(LinearLayout container, int idHint) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View childItem = inflater.inflate(R.layout.child_item, null);
+        TextView tvInput = childItem.findViewById(R.id.et_input_att);
+        tvInput.setHint(getString(idHint));
+        ImageView ivDelete = childItem.findViewById(R.id.add_btn_delete_phone);
+        ivDelete.setOnClickListener(delete -> {
+            container.removeView(childItem);
+        });
+        LinearLayout lnSelectTyp = childItem.findViewById(R.id.ln_select_type);
+        lnSelectTyp.setOnClickListener(show -> showDialog());
+        container.addView(childItem);
+    }
+
+    private void showDialog() {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View dialog = inflater.inflate(R.layout.diaglog_add_att, null );
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(dialog);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
